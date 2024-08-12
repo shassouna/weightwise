ecc8ecde61464e549c6f61b4b263a0e224244db1
Salim Hassouna <shassouna@decofinder.com>
2024-08-12 05:00:11 +0200

    add pg

--- config/database.js
+++ config/database.js
@@ -1,14 +1,17 @@
-// config/database.js
+// strapi-api/config/database.js
 module.exports = ({ env }) => ({
   connection: {
-    client: "mongo",
+    client: "postgres",
     connection: {
       host: env("DATABASE_HOST", "localhost"),
-      port: env.int("DATABASE_PORT", 27017),
-      database: env("DATABASE_NAME", "strapi"),
-      username: env("DATABASE_USERNAME", ""),
-      password: env("DATABASE_PASSWORD", ""),
-      ssl: env.bool("DATABASE_SSL", false),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "bank"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "0000"),
+      schema: env("DATABASE_SCHEMA", "public"), // Not required
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+      },
     },
     debug: false,
   },
